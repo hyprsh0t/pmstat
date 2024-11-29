c82e5a25fe8907c0c2fa952485912db35a4917f6
hyprsh0t <realfaith7777@gmail.com>
2024-11-29 12:37:53 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,40 @@ app = f(__name__)
 @app.route('/')
 def main():
     return """
-    <h1>Hello, Welcome to Proxbox</h1>
+    <h1>Hello, Welcome to Proxbox </h1>
     <p>Please check the proxbox documentation on GitHub to see what you want to do.</p>
     <p></p>
     <p>Blessings!</p>
 """
+
+@app.route('/', methods=['GET'])
+def getInfo(response):
+    information = {
+        "name": str(),
+        "cpuName": str(),
+        "memory": str(),
+        "disk": str(),
+        "network": str(),
+        "networkInterface": str(),
+        "ip": str(),
+        "os": str(),
+        "osVersion": str(),
+        "osDistribution": str(),
+        "gpuName": str(),
+        "gpuMemory": str(),
+        "gpuDriverVersion": str(),
+        "gpuDriverName": str(),
+        "gpuDriverDate": str(),
+        "gpuTemperature": str(),
+        "cpuFrequency": str(),
+        "cpuLoad": str(),
+        "cpuCores": str(),
+        "cpuThreads": str(),
+        "cpuUsage": str(),
+        "cpuVoltage": str(),
+        "cpuPower": str(),
+        "cpuTemperature": str(),
+    }
 
 
 
